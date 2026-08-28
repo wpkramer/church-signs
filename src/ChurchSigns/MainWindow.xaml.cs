76c6e0175c5139e989116a926645d090fb0f0143
William Kramer <wpkramer@yahoo.com>
2026-08-27 20:38:41 -0500

    Displayed signs from hardcoded test SignData

--- src/ChurchSigns/MainWindow.xaml.cs
+++ src/ChurchSigns/MainWindow.xaml.cs
@@ -137,7 +137,80 @@ namespace ChurchSigns
         public ObservableCollection<SignData> Signs { get; private set; }
         public ObservableCollection<SignTemplate> Templates { get; private set; }
 
-       
+
+        private void SignTemplatesListView_SelectionChanged(object sender, Microsoft.UI.Xaml.Controls.SelectionChangedEventArgs e)
+        {
+            
+            LoadSigns(SignTemplatesListView.SelectedItem);
+            ShowPasteArea(SignTemplatesListView.SelectedItem);
+        }
+
+        private void ShowPasteArea(object selectedItem)
+        {
+            
+        }
+
+        private void LoadSigns(object selectedItem)
+        {
+            if (selectedItem == null)
+                return;
+            
+            if(selectedItem is SignTemplate signTemplate)
+            {
+                Signs.Clear();
+                var sd = new SignData(signTemplate);
+                sd.Fields.Add("Name", "Kramer");
+                Signs.Add(sd);
+
+                for(int i = 0; i < 100; i++)
+                {
+                    sd = new SignData(signTemplate);
+                    sd.Fields.Add("Name", $"Jones #{i}");
+                    Signs.Add(sd);
+                }
+            }
+
+            //templateStorageItem.SvgSignTemplate = await FileIO.ReadTextAsync(svgFile);
+            //SignData templateStorageItem = new()
+            //{
+            //    Title = "Sign SvgSignTemplate"
+            //};
+
+            //await ReadContentFileAsync(templateStorageItem);
+
+            //Signs.Add(templateStorageItem);
+
+            //Signs.Add(new SignData
+            //{
+            //    Title = "William Kramer",
+            //    SvgSignTemplate = templateStorageItem.SvgSignTemplate,
+            //    Fields = new Dictionary<string, string>
+            //    {
+            //        { "Name", "William Kramer" }
+            //    }
+            //});
+            //Signs.Add(new SignData
+            //{
+            //    Title = "John Smith",
+            //    SvgSignTemplate = templateStorageItem.SvgSignTemplate,
+            //    Fields = new Dictionary<string, string>
+            //    {
+            //        { "Name", "Smith" }
+            //    }
+            //});
+            //for (int i = 0; i < 100; ++i)
+            //{
+            //    Signs.Add(new SignData
+            //    {
+            //        Title = $"Jones #{i}",
+            //        SvgSignTemplate = templateStorageItem.SvgSignTemplate,
+            //        Fields = new Dictionary<string, string>
+            //        {
+            //            { "Name", $"Jones{i}" }
+            //        }
+            //    });
+            //}
+        }
     }
 
    
